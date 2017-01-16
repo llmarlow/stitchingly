2dad38f5f53551bfc63aed76883c856c35c48c93
Lorna Marlow <lornamarlow@hotmail.co.uk>
2017-01-16 15:45:34 +0000

    Add header

--- db/schema.rb
+++ db/schema.rb
@@ -10,39 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170116112927) do
+ActiveRecord::Schema.define(version: 20170116144939) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "brands", force: :cascade do |t|
-    t.string   "name"
-    t.string   "website"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "flosses", force: :cascade do |t|
     t.string   "colour"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "projects", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer  "height"
-    t.integer  "width"
-    t.decimal  "price"
-    t.integer  "brand_id"
-    t.integer  "type_id"
-    t.index ["brand_id"], name: "index_projects_on_brand_id", using: :btree
-    t.index ["type_id"], name: "index_projects_on_type_id", using: :btree
-  end
-
-  create_table "types", force: :cascade do |t|
-    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -60,31 +34,8 @@ ActiveRecord::Schema.define(version: 20170116112927) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "username"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-    t.index ["username"], name: "index_users_on_username", unique: true, using: :btree
   end
 
-  create_table "wishlist_items", force: :cascade do |t|
-    t.integer  "project_id"
-    t.integer  "wishlist_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.index ["project_id"], name: "index_wishlist_items_on_project_id", using: :btree
-    t.index ["wishlist_id"], name: "index_wishlist_items_on_wishlist_id", using: :btree
-  end
-
-  create_table "wishlists", force: :cascade do |t|
-    t.integer  "user_id"
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_wishlists_on_user_id", using: :btree
-  end
-
-  add_foreign_key "projects", "brands"
-  add_foreign_key "projects", "types"
-  add_foreign_key "wishlist_items", "projects"
-  add_foreign_key "wishlist_items", "wishlists"
 end
