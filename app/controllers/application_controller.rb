8c1786e8bcfaea6ad2903b831650e82977a467cd
Lorna Marlow <lornamarlow@hotmail.co.uk>
2017-01-17 21:15:52 +0000

    Edit create quantity button on flosses index page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def after_sign_in_path_for(resource)
+    flosses_path
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+
+  def after_sign_up_path_for(resource)
+    flosses_path
+  end
+
 end
