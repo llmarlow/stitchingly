5d7981e63d0d7ba5e3591211ed147996caf1875e
Lorna Marlow <lornamarlow@hotmail.co.uk>
2017-01-23 10:21:05 +0000

    Add user dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def after_sign_in_path_for(resource)
-    flosses_path
+    root_path
   end
 
   def after_sign_out_path_for(resource)
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_up_path_for(resource)
-    flosses_path
+    root_path
   end
 
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,7 @@
 class Project < ApplicationRecord
   belongs_to :user
   validates :name, presence: true
+  validates :status, presence: true
   validates :name, uniqueness: true
   validates :user, presence: true
 end
