d2523db0690ca6f870f2aa664f557c52ca1379c6
Lorna Marlow <lornamarlow@hotmail.co.uk>
2017-01-25 16:23:44 +0000

    Update strong params for Devise

--- app/controllers/flosses_controller.rb
+++ app/controllers/flosses_controller.rb
@@ -1,6 +1,7 @@
 class FlossesController < ApplicationController
   def index
     @flosses = Floss.by_brand_and_type(params[:brand], params[:type])
-    @flosses = @flosses.paginate(:page => params[:page], :per_page => 60)
+    @q = @flosses.ransack(params[:q])
+    @flosses = @q.result.paginate(:page => params[:page], :per_page => 60)
   end
 end
