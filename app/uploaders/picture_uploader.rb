cc54be1c91c15afd6a95dd106f043963e6bb2311
Lorna Marlow <lornamarlow@hotmail.co.uk>
2017-02-07 14:13:36 +0000

    Auto orient images

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -6,13 +6,18 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   storage :fog
-
+  process :auto_orient
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  def auto_orient
+    manipulate! do |img|
+      img = img.auto_orient
+    end
+  end 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
