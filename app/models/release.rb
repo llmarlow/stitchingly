b1f49731dfa333c998f7ed103d3c782027b23fb2
Lorna Marlow <lornamarlow@hotmail.co.uk>
2017-04-21 14:06:02 +0000

    scope release name uniqueness to sal_id

--- app/models/release.rb
+++ app/models/release.rb
@@ -1,6 +1,6 @@
 class Release < ApplicationRecord
   belongs_to :sal
   mount_uploader :picture, PictureUploader
-  validates :name, uniqueness: true
+  validates :name, :uniqueness => {:scope=>:sal_id}
   validates :name, :picture, :sal, presence: true
 end
